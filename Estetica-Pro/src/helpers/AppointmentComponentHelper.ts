f4045e519e51c114d1fc0c6bd6ae9581399485c9
Sele <mailennso15@gmail.com>
2025-11-18 22:23:32 -0300

    corrección fecha

--- Estetica-Pro/src/helpers/AppointmentComponentHelper.ts
+++ Estetica-Pro/src/helpers/AppointmentComponentHelper.ts
@@ -28,7 +28,10 @@ export const formatDate = (date: Date) => {
 };
 
 export const formatDateISO = (date: Date) => {
-    return date.toISOString().split('T')[0];
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  return `${year}-${month}-${day}`;
 };
 
 export const getWeekDates = (currentDate) => {
